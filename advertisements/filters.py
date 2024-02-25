690c81080d1e3eb9d19a6f5d200698d9b36bc9d1
Anton Shubin <rtl8187b@gmail.com>
2024-02-26 00:24:36 +0600

    Initial commit

---
+++ advertisements/filters.py
@@ -0,0 +1,22 @@
+from django_filters import rest_framework as filters
+
+from advertisements.models import Advertisement
+
+
+STATUS_CHOICES = (
+    ("open", "OPEN"),
+    ("closed", "CLOSED")
+)
+
+
+class AdvertisementFilter(filters.FilterSet):
+    """Фильтры для объявлений."""
+
+    created_at = filters.DateFromToRangeFilter()
+    updated_at = filters.DateFilter()
+    status = filters.ChoiceFilter(choices=STATUS_CHOICES)
+    creator = filters.DjangoFilterBackend()
+
+    class Meta:
+        model = Advertisement
+        fields = ("created_at", "updated_at", "status", "creator")
---
+++ advertisements/views.py
@@ -0,0 +1,33 @@
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwner
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filterset_class = AdvertisementFilter
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+
+        # if self.action in ["create", "update", "partial_update", "destroy"]:
+        #     return [IsAuthenticated(), IsOwner()]
+        #     # permission_classes = [IsAuthenticated, IsOwner]
+        #     # return [permission() for permission in permission_classes]
+        # return []
+
+        if self.action in ("create", "update", "partial_update", "destroy"):
+            if self.action == "create":
+                self.permission_classes = [IsAuthenticated, IsOwner]
+            elif IsAdminUser():
+                self.permission_classes = [IsAuthenticated, IsAdminUser]
+            else:
+                self.permission_classes = [IsAuthenticated, IsOwner]
+        return super(self.__class__, self).get_permissions()
